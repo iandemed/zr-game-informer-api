c8bb89eab798a3fcb2c25f32e11e393ad3d5f94d
iandemed <iandemed@gmail.com>
2020-09-16 17:11:05 -0400

    Saved connnection.js file

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+let mongoURI = "";
+
+if (process.env.NODE_ENV === "production") {
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI = "mongodb://localhost/zr-review-api";
+}
