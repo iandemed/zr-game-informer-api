af29b0245c5aaf9ed9981adca5613f2c0e682da6
maurcord <67590886+maurcord@users.noreply.github.com>
2020-09-17 15:45:23 -0400

    updated game.js controller, removed routes from games.js route

--- routes/games.js
+++ routes/games.js
@@ -3,41 +3,12 @@ let router = new express.Router();
 const gamesController = require(`../controllers/game`);
 const Game = require("../models/Game");
 
+
 router.get("/", gamesController.index);
+router.get("/:id", gamesController.findById);
+router.post("/", gamesController.create);
+router.put("/:id", gamesController.editById);
+router.delete("/:id", gamesController.deleteById);
 
-
-router.get("/id/:id", (req, res) => {
-    Game.find({ index: req.params.index })
-      .populate("reviews")
-      .then((games) => res.json(games));
-  });
-
-  router.get("/index/:index", (req, res) => {
-  Game.find({ index: req.params.index })
-    .populate("reviews")
-    .then((games) => res.json(games));
-});
-
-router.post("/", (req, res) => {
-  Game.create(req.body).then((Game) => res.json(Game));
-});
-
-router.put("/index/:index", (req, res) => {
-  Game.findOneAndUpdate({ index: req.params.index }, req.body).then((games) => {
-    res.json(games);
-  });
-});
-
-router.delete("/index/:index", (req, res) => {
-  Game.findOneAndDelete({ index: req.params.index }).then((games) => {
-    res.json(games);
-  });
-});
-
-router.delete("/id/:id", (req, res) => {
-  Game.findOneAndDelete({ _id: req.params.id }).then((games) => {
-    res.json(games);
-  });
-});
 
 module.exports = router;
