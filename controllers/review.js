68f059baae2e7ed1285818ce441e025e501f5dc3
iandemed <iandemed@gmail.com>
2020-09-17 13:58:58 -0400

    Added basic routes to Review controller

--- controllers/review.js
+++ controllers/review.js
@@ -1,9 +1,29 @@
-const Review = require("../models/Game");
+const Review = require("../models/Review");
 
 module.exports = {
   index: (req, res) => {
     Review.find({}).then((reviews) => {
       res.json(reviews);
+    });
+  },
+  findById: (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+      res.json(review);
+    });
+  },
+  create: (req, res) => {
+    Review.create(req.body).then((review) => {
+      res.json(review);
+    });
+  },
+  editById: (req, res) => {
+    Review.findOneAndUpdate({ _id: req.params.id }, req.body).then((review) => {
+      res.json(review);
+    });
+  },
+  deleteById: (req, res) => {
+    Review.findOneAndDelete({ _id: req.params.id }, req.body).then((review) => {
+      res.json(review);
     });
   },
 };
