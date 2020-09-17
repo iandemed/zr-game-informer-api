d3b592a4e0c232fe9b6dd977ed068e61382f1f88
maurcord <67590886+maurcord@users.noreply.github.com>
2020-09-16 21:45:50 -0400

    added review.json

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,30 @@
 const mongoose = require("./connection");
+const Game = require('./models/Game');
+const Review = require('./models/Review');
+const games = require('./games.json');
+
+const newGameData = games.map((d) => {
+    let newReview = d.reviews.map((m)) = {
+    let 
+    
+    
+    
+   
+    
+
+ 
+  
+
+
+
+
+  // deletes data and starts fresh
+  Character.deleteMany({}).then(() => {
+    Character.create(newCharacterData)
+      .then((characters) => {
+        console.log(characters);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
