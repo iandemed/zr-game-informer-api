9979da415e7fab70f78cdfec2cbd671d584da85f
maurcord <67590886+maurcord@users.noreply.github.com>
2020-09-17 13:58:16 -0400

    added crud routes to games.js

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,36 @@
 const express = require("express");
 let router = new express.Router();
 const gamesController = require(`../controllers/game`);
+const Game = require("../models/Game");
 
 router.get("/", gamesController.index);
+
+router.get("/name/:name", (req, res) => {
+  Game.find({ name: req.params.name }).then((games) => res.json(games));
+});
+
+router.post("/", (req, res) => {
+  Game.create(req.body).then((Game) => res.json(Game));
+});
+
+router.put("/name/:name", (req, res) => {
+  Game.findOneAndUpdate({
+    name: req.params.name,
+  }).then((games) => {
+    res.json(games);
+  });
+});
+
+router.delete("/name/:name", (req, res) => {
+  Game.findOneAndDelete({ name: req.params.name }).then((games) => {
+    res.json(games);
+  });
+});
+
+router.delete("/id/:id", (req, res) => {
+  Game.findOneAndDelete({ _id: req.params.id }).then((games) => {
+    res.json(games);
+  });
+});
 
 module.exports = router;
