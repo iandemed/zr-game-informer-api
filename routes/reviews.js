6a9d3649f468fdc6047cb23814ec1860587b9de7
iandemed <iandemed@gmail.com>
2020-09-17 14:14:31 -0400

    Added tested basic routes for Review model

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,5 +3,9 @@ let router = new express.Router();
 const reviewsController = require(`../controllers/review`);
 
 router.get("/", reviewsController.index);
+router.get("/:id", reviewsController.findById);
+router.post("/", reviewsController.create);
+router.put("/:id", reviewsController.editById);
+router.delete("/:id", reviewsController.deleteById);
 
 module.exports = router;
