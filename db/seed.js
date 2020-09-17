2522d7c152021530245fd9a6a63e83971e779810
maurcord <67590886+maurcord@users.noreply.github.com>
2020-09-16 22:48:56 -0400

    modified seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,30 +1,17 @@
 const mongoose = require("./connection");
-const Game = require('./models/Game');
-const Review = require('./models/Review');
-const games = require('./games.json');
-
-const newGameData = games.map((d) => {
-    let newReview = d.reviews.map((m)) = {
-    let 
-    
-    
-    
-   
-    
-
- 
-  
+const Game = require("./models/Game");
+const Review = require("./models/Review");
+const gameData = require("./games.json");
 
 
 
 
-  // deletes data and starts fresh
-  Character.deleteMany({}).then(() => {
-    Character.create(newCharacterData)
-      .then((characters) => {
-        console.log(characters);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+Game.deleteMany({}).then(
+  Game.create(gameData)
+    .then(() => {
+      console.log(gameData);
+    })
+    .catch((err) => {
+      console.log("Failed to seed data.", err);
+    })
+);
