fbbc93a94ecf8310843bb6086081a1b99555a823
iandemed <iandemed@gmail.com>
2020-09-16 23:03:04 -0400

    Finish seed.js and add node command to begin seeding

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,10 @@
 const mongoose = require("./connection");
-const Game = require("./models/Game");
-const Review = require("./models/Review");
+
+const Game = require("../models/Game");
 const gameData = require("./games.json");
+
+const Review = require("../models/Review");
+const reviewData = require("./reviews.json");
 
 Game.deleteMany({}).then(
   Game.create(gameData)
