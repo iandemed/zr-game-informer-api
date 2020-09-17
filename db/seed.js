371fbc6fac02161c6ce3eb7136450970e74da715
maurcord <67590886+maurcord@users.noreply.github.com>
2020-09-16 22:56:06 -0400

    added Seed Review Data

--- db/seed.js
+++ db/seed.js
@@ -3,13 +3,20 @@ const Game = require("./models/Game");
 const Review = require("./models/Review");
 const gameData = require("./games.json");
 
-
-
-
 Game.deleteMany({}).then(
   Game.create(gameData)
     .then(() => {
       console.log(gameData);
+    })
+    .catch((err) => {
+      console.log("Failed to seed data.", err);
+    })
+);
+
+Review.deleteMany({}).then(
+  Review.create(reviewData)
+    .then(() => {
+      console.log(reviewData);
     })
     .catch((err) => {
       console.log("Failed to seed data.", err);
